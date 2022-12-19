b16cd213243de4f75b74d1545b93064726988444
elisa-tang <etang821@gmail.com>
2022-12-19 16:43:37 -0600

    Updated function scripts (NOTE: this will not be used, is made and kept for the purpose of testing request mappings with db functions.)

--- postgres/data.access/create_appraisal_methods.sql
+++ postgres/data.access/create_appraisal_methods.sql
@@ -1,23 +1,21 @@
 \connect "innovet"
 
-create or replace function ivh.get_appraisal_by_employee_id(pEmployeeId int)
+create or replace function ivh.get_appraisals_by_employee_id(pEmployeeId int)
     returns table
-    ( employee_id int
+    ( appraisal_id int
     , employee_name text
     , supervisor_name text
-    , appraisal_id int
     , year int
-    , status_id int
+    , status_code text
     , status text) 
 	language plpgsql as $$
     begin
         return query
-            select e.employee_id
+            select a.appraisal_id
                 , e.last_name || ', ' || e.first_name as employee_name
                 , s.last_name || ', ' || s.first_name as supervisor_name
-                , a.appraisal_id
                 , a.year
-                , a.status_id
+                , st.status_code
                 , st.status
             from ivh.employee e
                 join ivh.supervisor s on e.supervisor_id = s.supervisor_id
@@ -28,29 +26,25 @@ create or replace function ivh.get_appraisal_by_employee_id(pEmployeeId int)
 
 create or replace function ivh.get_goals_by_appraisal_id(appraisalId int)
     returns table
-    ( appraisal_id int
-    , employee_id int
-    , employee_name text
-    , year int
-    , status_id int
-    , status text
-    , goals_id int
+    ( goals_id int
     , goal_type text
     , goal text
+    , employee_name text
+    , year int
+    , status_code text
+    , status text
     , rating_code text
     , rating text) 
     language plpgsql as $$
     begin
         return query
-            select a.appraisal_id
-                , e.employee_id
-                , e.last_name || ', ' || e.first_name as employee_name
-                , a.year
-                , a.status_id
-                , st.status
-                , g.goal_id
+            select g.goal_id
                 , gt.goal_type
                 , g.goal
+                , e.last_name || ', ' || e.first_name as employee_name
+                , a.year
+                , st.status_code
+                , st.status
                 , r.rating_code
                 , r.rating
             from ivh.employee e
